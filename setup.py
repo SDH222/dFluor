e0569f33f2dfaf2cbf7c996234d829990f8bb14b
SDH222 <121490787+SDH222@users.noreply.github.com>
2024-12-13 14:51:45 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="dFluor", 
+    version="1.0.0",
+    packages=setuptools.find_packages(), 
+    description="package for fluorescence image analysis with differentiation",
+    author="Hiraku Suda",
+    python_requires='>=3.9',
+)
